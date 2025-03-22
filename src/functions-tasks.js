f0a21e8881ab4a52e2faa6c8ff3bf50098e44a7a
Meruert <mika.amantaikuzu@gmail.com>
2025-03-22 12:03:06 +0500

    feat: memoize done

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -115,8 +115,17 @@ function getPolynom(...args) {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+function memoize(func) {
+  const cache = [];
+
+  return () => {
+    if (cache.length === 0) {
+      const res = func();
+      cache.push(res);
+      return res;
+    }
+    return cache.at(-1);
+  };
 }
 
 /**
